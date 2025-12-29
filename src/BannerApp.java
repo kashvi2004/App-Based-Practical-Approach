55fa671b1ee3ec055c49826dd6627959bcb8878b
Kashvi Sharma <codingspace99@gmail.com>
2025-12-29 11:04:16 +0530

    Initial commit

---
+++ src/BannerApp.java
@@ -0,0 +1,13 @@
+/*
+OOPS banner app -
+@author Kashvi
+@version 1.0
+ */
+public class BannerApp {
+
+    public static void main(String[] args) {
+
+        System.out.println("OOPS");
+
+    }
+}
